e6dc12cd7e5b714a9fff0ca398a6fcbf4d3da764
Rafael R Nakano <lazpeng@gmail.com>
2016-08-18 17:26:58 -0300

    Initial translation process

---
+++ _includes/pt-BR/example.rs
@@ -0,0 +1,23 @@
+// Esse código é editável e rodável!
+fn main() {
+    // Uma calculadora simples de inteiros:
+    // `+` or `-` significa somar ou subtrair 1
+    // `*` or `/` significa multiplicar ou dividir por 2
+
+    let programa = "+ + * - /";
+    let mut acumulador = 0;
+
+    for simbolo in programa.chars() {
+        match simbolo {
+            '+' => acumulador += 1,
+            '-' => acumulador -= 1,
+            '*' => acumulador *= 2,
+            '/' => acumulador /= 2,
+            _ => {
+                // ignore todo o resto
+            }
+        }
+    }
+
+    println!("O programa \"{}\" gera o valor {}", programa, acumulador);
+}
