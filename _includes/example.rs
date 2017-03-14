1608e32bb5c77ee1760af9e27e3071d4de4ab2d2
Esteban Echeverry <eecheverry@nubark.com>
2017-03-13 20:22:11 -0500

    Change example.rs to a simpler and more welcoming one

--- _includes/example.rs
+++ _includes/example.rs
@@ -1,22 +1,7 @@
 // This code is editable and runnable!
 fn main() {
-    // A simple integer calculator:
-    // `+` or `-` means add or subtract by 1
-    // `*` or `/` means multiply or divide by 2
-
-    let program = "+ + * - /";
-    let mut accumulator = 0;
-
-    for token in program.chars() {
-        match token {
-            '+' => accumulator += 1,
-            '-' => accumulator -= 1,
-            '*' => accumulator *= 2,
-            '/' => accumulator /= 2,
-            _ => { /* ignore everything else */ }
-        }
-    }
-
-    println!("The program \"{}\" calculates the value {}",
-              program, accumulator);
+    println!("Hello");
+    println!("Hola");
+    println!("こんにちは");
+    println!("您好");
 }
