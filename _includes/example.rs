8a6373e5b7e0e9e7d197563903d3f1223151646c
Esteban Echeverry <eecheverry@nubark.com>
2017-03-15 21:37:29 -0500

    Make example use an array and a for loop

--- _includes/example.rs
+++ _includes/example.rs
@@ -1,7 +1,8 @@
 // This code is editable and runnable!
 fn main() {
-    println!("Hello");
-    println!("Hola");
-    println!("こんにちは");
-    println!("您好");
+    let greetings = ["Hello", "Hola", "こんにちは", "您好"];
+
+    for greeting in greetings.iter() {
+      println!("{}", greeting);
+    }
 }
