2b8324cbfcb6a8c01f15fd87556f1506d2531fab
Fabian Knorr <fbknorr@gmail.com>
2017-10-04 18:02:51 +0200

    Move German greeting to the end of the example code

--- _includes/example.rs
+++ _includes/example.rs
@@ -1,24 +1,24 @@
 fn main() {
-    let greetings = ["Hallo", "Hello", "Hola", "Bonjour",
+    let greetings = ["Hello", "Hola", "Bonjour",
                      "Ciao", "こんにちは", "안녕하세요",
                      "Cześć", "Olá", "Здравствуйте",
-                     "Chào bạn", "您好"];
+                     "Chào bạn", "您好", "Hallo"];
 
     for (num, greeting) in greetings.iter().enumerate() {
         print!("{} : ", greeting);
         match num {
-            0 =>  println!("Dieser Code kann bearbeitet und ausgeführt werden!"),
-            1 =>  println!("This code is editable and runnable!"),
-            2 =>  println!("¡Este código es editable y ejecutable!"),
-            3 =>  println!("Ce code est modifiable et exécutable !"),
-            4 =>  println!("Questo codice è modificabile ed eseguibile!"),
-            5 =>  println!("このコードは編集して実行出来ます！"),
-            6 =>  println!("여기에서 코드를 수정하고 실행할 수 있습니다!"),
-            7 =>  println!("Ten kod można edytować oraz uruchomić!"),
-            8 =>  println!("Esse código é editável e executável!"),
-            9 =>  println!("Этот код можно отредактировать и запустить!"),
-            10 => println!("Bạn có thể edit và run code trực tiếp!"),
-            11 => println!("这段代码是可以编辑并且能够运行的！"),
+            0 =>  println!("This code is editable and runnable!"),
+            1 =>  println!("¡Este código es editable y ejecutable!"),
+            2 =>  println!("Ce code est modifiable et exécutable !"),
+            3 =>  println!("Questo codice è modificabile ed eseguibile!"),
+            4 =>  println!("このコードは編集して実行出来ます！"),
+            5 =>  println!("여기에서 코드를 수정하고 실행할 수 있습니다!"),
+            6 =>  println!("Ten kod można edytować oraz uruchomić!"),
+            7 =>  println!("Esse código é editável e executável!"),
+            8 =>  println!("Этот код можно отредактировать и запустить!"),
+            9 =>  println!("Bạn có thể edit và run code trực tiếp!"),
+            10 => println!("这段代码是可以编辑并且能够运行的！"),
+            11 => println!("Dieser Code kann bearbeitet und ausgeführt werden!"),
             _ =>  {},
         }
     }
