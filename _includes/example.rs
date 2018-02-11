6ce97517979cb368d36f143ea52a18952a0208fe
TheRealChandu <therealchandu@gmail.com>
2018-02-11 23:56:40 +0530

    Update example.rs
    Added Malayalam language greetings.

--- _includes/example.rs
+++ _includes/example.rs
@@ -3,7 +3,7 @@ fn main() {
                      "Ciao", "こんにちは", "안녕하세요",
                      "Cześć", "Olá", "Здравствуйте",
                      "Chào bạn", "您好", "Hallo",
-                     "Hej", "Ahoj", "سلام"];
+                     "Hej", "Ahoj", "سلام", "നമസ്കാരം"];
 
     for (num, greeting) in greetings.iter().enumerate() {
         print!("{} : ", greeting);
@@ -23,6 +23,7 @@ fn main() {
             12 => println!("Den här koden kan redigeras och köras!"),
             13 => println!("Tento kód můžete upravit a spustit"),
             14 => println!("این کد قابلیت ویرایش و اجرا دارد!"),
+            15 => println!("ഈ കോഡ് തിരുത്താനും പ്രവർത്തിപ്പിക്കാനും സാധിക്കുന്നതാണ്!"),
             _ =>  {},
         }
     }
